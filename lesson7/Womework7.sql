3c8973723ffdd82f0b771ae5afe0cfe85aa361f0
Ekaterina <esokolova@mindthedigital.com>
2020-10-09 00:29:59 +0300

    Not finished task 3

--- lesson7/Womework7.sql
+++ lesson7/Womework7.sql
@@ -14,4 +14,34 @@ name,
  (SELECT name FROM catalogs WHERE id = p.catalog_id) AS catalog_names
 FROM products p;
 
--- 3 (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
+-- 3 (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
+-- Поля from, to и label содержат английские названия городов, поле name — русское. 
+-- Выведите список рейсов flights с русскими названиями городов.
+
+
+DROP TABLE IF EXISTS flights;
+CREATE TABLE flights (
+	id SERIAL PRIMARY KEY,
+    frm VARCHAR(50), -- eng
+    to_city VARCHAR(50) -- eng
+);
+
+INSERT INTO flights (id, frm, to_city) values 
+	(1, 'Moscow', 'Sochi'), 
+    (2, 'Paris', 'Lyon'), 
+    (3, 'Dresden', 'Munich'), 
+    (4, 'London', 'Paris'), 
+    (5, 'Moscow', 'Stambul');
+    
+CREATE TABLE IF NOT EXISTS cities (
+	label VARCHAR(50), -- eng
+    name VARCHAR(50) -- ru
+);
+
+INSERT INTO cities (label, name) values 
+	('Moscow', 'Москва'), ('Sochi', 'Сочи'), 
+    ('Paris', 'Париж'), ('Lyon', 'Лион'), 
+    ('London', 'Лондон'), ('Dresden', 'Дрезден'), 
+    ('Munich', 'Мюнхен'), ('Stambul', 'Стамбул');
+
+
