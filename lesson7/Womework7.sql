7e764cfee1c0e9a8ade2643406ff4a4ff056fdae
Ekaterina <esokolova@mindthedigital.com>
2020-10-08 22:01:22 +0300

    Add task 2

--- lesson7/Womework7.sql
+++ lesson7/Womework7.sql
@@ -9,6 +9,9 @@ WHERE EXISTS (SELECT * FROM orders WHERE user_id = users.id);
 
 -- 2 Выведите список товаров products и разделов catalogs, который соответствует товару.
 
-SELECT * FROM products
+SELECT id, 
+name,
+ (SELECT name FROM catalogs WHERE id = p.catalog_id) AS catalog_names
+FROM products p;
 
 -- 3 (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
