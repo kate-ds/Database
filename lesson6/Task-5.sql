5b7c7161d21f7a52a2f2dea5237721191a1ba23b
Ekaterina <esokolova@mindthedigital.com>
2020-10-09 11:44:35 +0300

    Add task 5

--- lesson6/Task-5.sql
+++ lesson6/Task-5.sql
@@ -1,13 +1,26 @@
+
 -- Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети.
 
--- можно посчитать сумму поставленных лайков, сумму постов, сумму комментов, взять среднее
+-- можно посчитать сумму поставленных лайков, сумму постов, сумму комментов, посчитать общую сумму
 
 SELECT id,
-	CONCAT(name, ' ', surname) user
-    -- () posted
-    -- (union) liked
-    -- () commented
-    -- () activity
-FROM users 
--- ORDER BY activity
--- LIMIT 10; 
+	CONCAT(name, ' ', surname) user,
+    IFNULL((SELECT COUNT(*) FROM posts WHERE user_id = u.id GROUP BY user_id), 0) `posted`,
+    IFNULL((SELECT COUNT(*) FROM likes WHERE from_user_id = u.id GROUP BY from_user_id), 0) `liked`,
+    IFNULL((SELECT COUNT(*) FROM comments WHERE user_id = u.id GROUP BY user_id), 0) `commented`,
+    ((IFNULL((SELECT COUNT(*) FROM posts WHERE user_id = u.id GROUP BY user_id), 0)) + 
+    (IFNULL((SELECT COUNT(*) FROM likes WHERE from_user_id = u.id GROUP BY from_user_id), 0)) + 
+    (IFNULL((SELECT COUNT(*) FROM comments WHERE user_id = u.id GROUP BY user_id), 0))) as activity
+FROM users u
+ORDER BY activity
+LIMIT 10;
+
+
+-- posted (user id=1)
+SELECT COUNT(*) FROM posts WHERE user_id = 1 GROUP BY user_id;
+
+-- liked (user id=1)
+SELECT COUNT(*) FROM likes WHERE from_user_id in (1, 2, 3, 45, 56, 67, 105) AND IFNULL(from_user_id, 0) GROUP BY from_user_id;
+
+-- commented (user id=1)
+SELECT COUNT(*) FROM comments WHERE user_id = 1 GROUP BY user_id;
